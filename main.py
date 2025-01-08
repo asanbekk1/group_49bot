55c9b25ab4715d50b1dd78a59658264e19f4b4b5
aidarjeenbekov <aidarjeenbekov444@gmail.com>
2025-01-08 21:12:29 +0600

    main.py

--- main.py
+++ main.py
@@ -3,34 +3,23 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from dotenv import dotenv_values
 import logging
-
-token = dotenv_values(".env")["BOT_TOKEN"]
-bot = Bot(token=token)
-dp = Dispatcher()
-
-
-@dp.message(Command("start"))
-async def start_handler(message: types.Message):
-    name = message.from_user.first_name
-    # message.from_user.id
-    await message.answer(f"Привет, {name}")
-
-from dotenv import dotenv_values
-from aiogram import Bot, Dispatcher, types
-from asyncio import run
-from aiogram.filters import Command
 import random
 
 token = dotenv_values(".env")["BOT_TOKEN"]
 bot = Bot(token=token)
 dp = Dispatcher()
 
+
 names = ('name','aidar','igor')
 
 
 @dp.message(Command("start"))
-async def start(message: types.Message):
-    await message.answer(f'hello{message.from_user.first_name}')
+async def start_handler(message: types.Message):
+    name = message.from_user.first_name
+    # message.from_user.id
+    # await message.answer(f"Привет, {name}")
+    await message.reply(f"Привет, {name}")
+
 
 
 @dp.message(Command("my_info"))
@@ -48,25 +37,6 @@ async def main():
 
 
 if __name__ == '__main__':
-    run(main())
-
-
-async def main():
-    await dp.start_polling(bot)
-
-
-
-
-
-if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
-
-
-
-
-
-
-
-
 
