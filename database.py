a6340ad6ff272eb2cbc7bb401eb2831a830940bc
aidarjeenbekov <aidarjeenbekov444@gmail.com>
2025-01-18 18:52:01 +0600

    main.py

--- database.py
+++ database.py
@@ -1,26 +1,25 @@
 import sqlite3
 
+
 class Database:
-def __init__():
-    conn = sqlite3.connect('reviews.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS reviews (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER,
-        name TEXT,
-        instagram_username TEXT,
-        rating INTEGER,
-        extra_comments TEXT,
-        visit_date TEXT
-    )''')
+    def __init__(self):
+        conn = sqlite3.connect('reviews.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS reviews (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            name TEXT,
+            instagram_username TEXT,
+            rating INTEGER,
+            extra_comments TEXT,
+            visit_date TEXT
+        )''')
 
-def save_review(data, visit_date):
-    conn = sqlite3.connect('reviews.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO reviews (name, instagram_username, rating, extra_comments, visit_date)
-    VALUES (?, ?, ?, ?, ?)
-    ''', (data['name'], data['instagram_username'], data['rating'], data['extra_comments'], visit_date))
-
-
+    def save_review(data, visit_date):
+        conn = sqlite3.connect('reviews.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+        INSERT INTO reviews (name, instagram_username, rating, extra_comments, visit_date)
+        VALUES (?, ?, ?, ?, ?)
+        ''', (data['name'], data['instagram_username'], data['rating'], data['extra_comments'], visit_date,), )
