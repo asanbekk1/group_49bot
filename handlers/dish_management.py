4e4a2565b34670cebd116a0bafdb3dd0e7ad329f
aidarjeenbekov <aidarjeenbekov444@gmail.com>
2025-01-27 15:29:39 +0600

    main.py

--- handlers/dish_management.py
+++ handlers/dish_management.py
@@ -84,7 +84,7 @@ async def process_portion_sizes(m: types.Message, state: FSMContext):
 @dish_router.message(DishForm.photo)
 async def process_photo(m: types.Message, state: FSMContext):
     if m.photo:
-        file = await m.photo[1].download()
+        file = await m.photo[-1].download()
         photo_path = file.name
 
         await state.update_data(photo=photo_path)
